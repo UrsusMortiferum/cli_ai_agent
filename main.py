c2693f593cb866d8e6ebf7e9091e198153298ba2
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-12-06 01:29:48 +0100

    Added verbose flag

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from call_function import available_functions
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("prompt", type=str, help="User prompt")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -19,15 +20,24 @@ def main():
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print("User prompt:", args.user_prompt)
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.prompt,
+        contents=messages,
     )
     if not response.usage_metadata:
         raise RuntimeError("Probable failed API request")
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
     print("Response:")
     print(response.text)
 
